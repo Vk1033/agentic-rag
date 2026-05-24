bb778f99698a18ca84b76e3c75c491b178efecf4
Vk1033 <kinkarvansh@gmail.com>
2026-05-24 07:31:48 +0000

    feat: implement RAG document ingestion pipeline and add tiktoken dependency

---
+++ ingestion.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_nvidia import NVIDIAEmbeddings
+
+load_dotenv()
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=2000,
+    chunk_overlap=0,
+)
+
+splitted_docs = text_splitter.split_documents(docs_list)
+
+embedding = NVIDIAEmbeddings(model="nvidia/nv-embed-v1", truncate="NONE")
+
+# vectorstore = Chroma.from_documents(
+#     documents=splitted_docs,
+#     embedding=embedding,
+#     collection_name="rag-chroma",
+#     persist_directory="./.chroma",
+# )
+
+retriever = Chroma(
+    collection_name="rag-chroma",
+    persist_directory="./.chroma",
+    embedding_function=embedding,
+).as_retriever()
